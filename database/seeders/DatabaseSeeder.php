834b3573500dc4dfb3815c17cf846a33b82ddfa5
Nafishsy <nafizahmed273273@gmail.com>
2022-07-15 16:58:18 +0600

    Billing details done, careful seeding is needed, did a change in migration to test billing details, need to make c_id foriegn K of accounts table id

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,27 +1,30 @@
 <?php
- 
+
 namespace Database\Seeders;
- 
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
- 
+
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeders.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        
-        for($i=1;$i<=50;$i++){
+        $arr=array("Active","Inactive","Banned");
+        $bool=array(TRUE,FALSE);
+        for($i=1;$i<=1000;$i++){
 
             DB::table('customers')->insert([
-                'payement' => TRUE,
-                'status' => 'Active',
+                'payement' => $bool[array_rand($bool,1)],
+                'PayementDate' => date('Y-m-d H:i:s'),
+                'status' => $arr[array_rand($arr,1)],
                 'c_id' => $i,
             ]);
 
