bd3de41c44ee60e78f1b4e36985657125e1f51c7
Nafishsy <nafizahmed273273@gmail.com>
2022-07-20 00:27:12 +0600

    saving my presnt work just in case

--- resources/views/SubAdmin/MovieWatch.blade.php
+++ resources/views/SubAdmin/MovieWatch.blade.php
@@ -7,8 +7,9 @@ h4{
 p{
     color: #999;
 }
+
  </style>
 <video controls src="{{asset('movies')}}/{{$movie->movie}}" width="100%" height="50%"></video>
-<h4>{{$movie->name}}</h4>
+Name:<h4>{{$movie->name}}</h4>
 <img src="{{asset('banners')}}/{{$movie->banner}}" width="20%" height="25%" alt="{{$movie->name}}">
-<p>{{$movie->description}}</p>
+Description:<p>{{$movie->description}}</p>
