115278eaafde81f72e4f6407715ac45f15cd0ff9
Nafishsy <nafizahmed273273@gmail.com>
2022-07-19 22:45:26 +0600

    watch movie updated

--- resources/views/SubAdmin/MovieWatch.blade.php
+++ resources/views/SubAdmin/MovieWatch.blade.php
@@ -1,4 +1,14 @@
 <style>
 body {background-color: #000;}
+h4{
+    color: #999;
+    margin:0px;
+}
+p{
+    color: #999;
+}
  </style>
-<video controls src="{{asset('movies')}}/{{$movie->movie}}" width="100%" height="100%"></video>
+<video controls src="{{asset('movies')}}/{{$movie->movie}}" width="100%" height="50%"></video>
+<h4>{{$movie->name}}</h4>
+<img src="{{asset('banners')}}/{{$movie->banner}}" width="20%" height="25%" alt="{{$movie->name}}">
+<p>{{$movie->description}}</p>
