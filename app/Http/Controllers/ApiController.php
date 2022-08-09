bbb2f42fd385ea0eb408dae7083b8878b2034dd4
Nafishsy <nafizahmed273273@gmail.com>
2022-08-09 08:53:13 +0600

    making changes for frontend

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,4 +16,40 @@ class ApiController extends Controller
         return $data;
     }
 
+    public function UploadMovie(Request $req)
+    {
+
+        $this->validate($req,
+            [
+                "name"=>"required|min:5",
+                "description"=>"required|min:10|max:2000",
+                "genre"=>"required|in:Action,Thriller,Comedy,Adventure,Documentary",
+                "movie"=>"required|mimes:mp4,m4vm,mov,mkv",
+                "banner"=>"required|mimes:img,jpeg,gif,png"
+
+            ],
+        
+            [
+                "description.required"=>"Must write something about the movie",
+                "genre.required"=>"Must select the movie genre",
+                "movie.mimes"=>"Please select the valid file type which are mp4,m4vm,mov,mkv"
+                
+            ]);
+
+
+            $movie = $req->name.".".$req->file('movie')->getClientOriginalExtension();
+            $req->file('movie')->storeAs('movies',$movie);
+
+            $banner = $req->name.".".$req->file('banner')->getClientOriginalExtension();
+            $req->file('banner')->storeAs('banners',$banner);
+
+            $movies = new Movies;
+            $movies->name = $req->name;
+            $movies->description = $req->description;
+            $movies->genre = $req->genre;
+            $movies->movie = $movie;
+            $movies->banner = $banner;
+            $movies->save();
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/movies',[ApiController::class,'ManageMovies']);
+Route::get('/movie',[ApiController::class,'ManageMovies']);
+Route::post('/movie/create',[ApiController::class,'upload']);
